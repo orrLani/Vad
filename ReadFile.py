55688f9d976c6aac5f97c6ffb360d854587c8136
orrlani <olaniado@campus.technion.ac.il>
2020-03-20 22:59:35 +0200

    firstProgrem

---
+++ ReadFile.py
@@ -0,0 +1,16 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+
+scope=['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+cred=ServiceAccountCredentials.from_json_keyfile_name('onedrive.json',scope)
+
+gc = gspread.authorize(cred)
+
+wks = gc.open('Courses').sheet1
+mydata=wks.get_all_values()
+myHead=mydata.pop(0)
+df=pd.DataFrame(data=mydata,columns=myHead)
+print(df)
+print(type(df))
--- mainPorject.py
+++ mainPorject.py
@@ -114,7 +114,7 @@ class MyFrame(wx.Frame):
             if creds and creds.expired and creds.refresh_token:
                   creds.refresh(Request())
             else:
-                  flow = InstalledAppFlow.from_client_secrets_file('credentials/credentials.json', SCOPES)
+                  flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
                   creds = flow.run_local_server(port=0)
               # Save the credentials for the next run
             with open('token.pickle', 'wb') as token:
